b9616706672b276493d0f2378fa03b873d673bf2
its-aniket <aniketjadhav770698@gmail.com>
2024-08-25 23:24:50 +0530

    data transformation added

--- src/exception.py
+++ src/exception.py
@@ -19,9 +19,9 @@ class CustomeException(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by zero error")
-        raise CustomeException(e,sys)
+# if __name__ == "__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divided by zero error")
+#         raise CustomeException(e,sys)
